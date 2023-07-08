4646602a82803292db1b4330f0ba634ddf930cba
Tristan Zander <tristannzander@gmail.com>
2023-07-07 22:37:02 -0400

    Remove eventing

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -2,13 +2,8 @@
 
 #[macro_use]
 extern crate serde;
-#[macro_use]
-extern crate anyhow;
-#[macro_use]
-extern crate async_trait;
 
 pub mod config;
-pub mod eventing;
 pub mod logging;
 
 // TODO: Write common logger setup
